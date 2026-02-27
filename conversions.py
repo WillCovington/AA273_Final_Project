5c569d2da9b956241da248a4bdd079f19f10e5ea
Will <wkcovington@gmail.com>
2026-02-27 15:48:54 -0800

    added a generic conversions file with nothing in it, starting work on ekf function

---
+++ conversions.py
@@ -0,0 +1,3 @@
+# file for general conversions, like from lclf to lci or ecef to eci or yadda yadda
+
+def lunar_latlong_to_xyz(lat, lon, alt, R)):
--- ekf/ekf_process.py
+++ ekf/ekf_process.py
@@ -1,2 +1,26 @@
 # running through some function ideas
+import numpy as np
+
+def calculate_A_finite_diff(dt):
+    # still need to find out what this process is
+    A = np.eye(6)
+    A[0:3, 3:6] = np.eye(3) * dt
+    # this is where the hard part of finding A is
+    pass
+
+def calculate_A_analytical(dt):
+    # the (maybe) trickier way of finding A
+    A = np.eye(6)
+    A[0:3, 3:6] = np.eye(3) * dt
+
+    # general steps
+    # pass in truncation degree
+    # compute the potential map (call upon generate_potential)
+    # convert the potential from latitude, longitude, radius to xyz
+    # I think this part might just be a bitch regardless
+    pass
+
+
+
+
 
